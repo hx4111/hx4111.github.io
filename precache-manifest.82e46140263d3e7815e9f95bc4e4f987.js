774ca7e96ffcf00800d87ac8d0363ad63255c1b1
haoxu <haoxu@onething.net>
2020-04-01 18:18:35 +0800

    first commit

---
+++ precache-manifest.82e46140263d3e7815e9f95bc4e4f987.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd933fe38d0871d507f2ed42d144e10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97ee4da90b8bcbe6d34c",
+    "url": "/static/css/2.cddfaa18.chunk.css"
+  },
+  {
+    "revision": "f7ec4badf600f340ab3c",
+    "url": "/static/css/main.1150bf6d.chunk.css"
+  },
+  {
+    "revision": "97ee4da90b8bcbe6d34c",
+    "url": "/static/js/2.26b631f1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.26b631f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7ec4badf600f340ab3c",
+    "url": "/static/js/main.2972190f.chunk.js"
+  },
+  {
+    "revision": "f348634ce247c3999240",
+    "url": "/static/js/runtime-main.fd07f623.js"
+  }
+]);
